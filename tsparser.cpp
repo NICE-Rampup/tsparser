d328b6a74286254c1cdc0b13066144aa8254bcaa
therishitakerr <75129590+therishitakerr@users.noreply.github.com>
2022-05-12 12:22:07 +0530

    Update tsparser.cpp

--- tsparser.cpp
+++ tsparser.cpp
@@ -1,9 +1,6 @@
-/*
-
-*/
-
 #include "header/convert.h"
 #include "header/tsheader.h"
+#include "header/checkfile.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -14,20 +11,29 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int count = 0;
-		fstream file(argv[1], ios::in | ios::binary);
-		while (!file.eof())
+		string fname = argv[1];
+		ifstream file(fname, ios::in | ios::binary);
+		if (check(file, fname))
 		{
-			int h;
-			++count;
-			file.read(reinterpret_cast<char *>(&h), sizeof(int));
-			string s = to_string(h);
-			char const *pchar = s.c_str();
-			Header h1(pchar, sizeof(pchar));
-			h1.display();
-			file.ignore(184); // ignores payload
+			int count = 0;
+			while (!file.eof())
+			{
+				int h;
+				++count;
+				file.read(reinterpret_cast<char *>(&h), sizeof(int));
+				string s = to_string(h);
+				char const *pchar = s.c_str();
+				Header h1(pchar, sizeof(pchar));
+				h1.display();
+				file.ignore(184); // ignores payload
+			}
+			cout << "no of packets: " << count << endl;
 		}
-		cout << "no of packets: " << count << endl;
+		else
+		{
+			cout<<"invalid file"<<endl;
+			return 0;
+		}
 	}
 
 	return 0;
@@ -38,5 +44,4 @@ uint32_t myint;
 while(fstr.read(reinterpret_cast<char*>(&myint), sizeof(myint))) {
 	// do something with myint
 }
-
 */
